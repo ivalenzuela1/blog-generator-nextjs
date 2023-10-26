4a8179a1e2bf1a14a3f8ce04a1f375a6e266083f
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-26 14:53:22 -0700

    Implement Auth0 and get user login state

--- blog-generator/app/page.tsx
+++ blog-generator/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import Link from "next/link";
 import React from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
   return (
     <main>
       <h1>Blog</h1>
+      {user && <div>The user {user?.name} is logged in</div>}
+      {user ? (
+        <a href="/api/auth/logout">Logout</a>
+      ) : (
+        <a href="/api/auth/login">Login</a>
+      )}
     </main>
   );
 }
