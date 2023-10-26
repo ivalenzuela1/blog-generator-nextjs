a74c7ac74766c214654ef22902bb2e506306717d
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-26 14:22:14 -0700

    Delete src directory. Add user page.

---
+++ blog-generator/app/api/test/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/mongo";
+
+export async function GET(request: NextRequest, response: NextResponse) {
+  ///const { db } = await connectToDatabase();
+  try {
+    //  const test = await db.collection("test").find({}).toArray();
+    //  return NextResponse.json({ message: "Hello World", data: test }, { status: 200 });
+    return NextResponse.json({ message: "Hello World" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
