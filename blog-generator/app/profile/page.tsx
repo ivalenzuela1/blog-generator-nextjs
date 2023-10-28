dd45bdb961ec384bd13661ec2c4385f6e1c586f2
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-28 13:57:34 -0700

    Implement profile page

--- blog-generator/app/profile/page.tsx
+++ blog-generator/app/profile/page.tsx
@@ -4,12 +4,25 @@ import Link from "next/link";
 import React from "react";
 
 export default function Profile() {
+  const addCredits = () => {
+    console.log("Add credits");
+  };
+
   return (
-    <div>
-      <h2>Profile</h2>
-      <div>
-        <Link href="/">Go to Home</Link>
-      </div>
-    </div>
+    <section className="w-full flex flex-col items-center">
+      <section className="w-[95%] max-w-4xl flex flex-col items-center gap-4"></section>
+      <h1 className="mt-4 text-4xl font-bold text-center text-indigo-600">
+        Profile
+      </h1>
+      <h2 className="text-2xl font-bold text-center text-gray-800">
+        You have 0 credits.
+      </h2>
+      <button
+        onClick={addCredits}
+        className="rounded-md px-4 py-2 cursor-pointer bg-indigo-600 text-white text-xl font-bold"
+      >
+        Buy more credits
+      </button>
+    </section>
   );
 }
