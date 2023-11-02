d6158b913dfbbd4b789a6ef6b0999fea3e696b4d
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-11-01 17:21:43 -0700

    add all of open ai logic

--- blog-generator/app/api/posts/generatePost/route.ts
+++ blog-generator/app/api/posts/generatePost/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { connectToDatabase } from "@/lib/mongo";
 import OpenAI from "openai";
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 // Use withApiAuthRequired to ensure a user session exists
 const withApiAuthRequiredExtended = withApiAuthRequired as any;
@@ -19,104 +19,6 @@ export const POST = withApiAuthRequiredExtended(
           { status: 500 }
         );
         //  return NextResponse.error();
-      }
-
-      const profile = await db
-        .collection("profiles")
-        .find({
-          uid: user.sub,
-        })
-        .toArray();
-
-      if (profile[0].credits < 1) {
-        return NextResponse.json(
-          {
-            success: false,
-            message: "Not enough credits",
-          },
-          { status: 200 }
-        );
-      }
-
-      const body = await request.json();
-      const { description, keywords, tone, title } = body as PostPrompt;
-
-      const openai = new OpenAI({
-        apiKey: process.env.OPENAI_API_KEY as string,
-      });
-
-      const generateTitle = await openai.chat.completions.create({
-        messages: [
-          { role: "system", content: "You are a blog post writer" },
-          {
-            role: "user",
-            content: `Write me a title for a blog post about ${description}. The keywords for the post are as follows: ${keywords}. The tone of the post should be ${tone}. The title should be SEO friendly and no longer than 15 words. Write only one title. ${
-              title.length > 0
-                ? `Take that title into consideration: ${title}.`
-                : ""
-            }}. Do not wrap the title in quotes.`,
-          },
-        ],
-        model: "gpt-3.5-turbo",
-        temperature: 0.2,
-      });
-
-      const titleResponse = generateTitle.choices[0].message.content;
-
-      const post: Post = {
-        title: titleResponse || "Test Title",
-        content: ["Test Content", JSON.stringify(body)],
-        uid: "123456789",
-      };
-
-      return NextResponse.json({ success: true, post }, { status: 200 });
-    } catch (error) {
-      return NextResponse.json({ message: "FAILURE" }, { status: 500 });
-      // return NextResponse.error();
-    }
-  }
-);
-
-/*
-import { NextRequest, NextResponse } from "next/server";
-import { connectToDatabase } from "@/lib/mongo";
-import OpenAI from "openai";
-import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
-
-// Use withApiAuthRequired to ensure a user session exists
-const withApiAuthRequiredExtended = withApiAuthRequired as any;
-
-export const POST = withApiAuthRequiredExtended(
-  async (request: NextRequest, response: NextResponse) => {
-    try {
-      const dbObject = await connectToDatabase();
-      console.log(dbObject.db);
-      console.log(dbObject.client);
-    } catch (e) {
-      return NextResponse.json(
-        { message: `Error: Client in DB: ${JSON.stringify(e)} ` },
-        { status: 500 }
-      );
-    }
-
-    const { db } = await connectToDatabase();
-    try {
-      const session = await getSession(request, response);
-      const user = session?.user;
-
-      if (!user) {
-        return NextResponse.json(
-          { message: "Error: No User" },
-          { status: 500 }
-        );
-        //  return NextResponse.error();
-      }
-
-      if (!db) {
-        return NextResponse.json(
-          { message: "Error: database" },
-          { status: 500 }
-        );
       }
 
       const profile = await db
@@ -186,8 +88,11 @@ export const POST = withApiAuthRequiredExtended(
         uid: user.sub,
       };
 
+      /*
+
       // Add post to database
       await db.collection("posts").insertOne(post);
+
 
       // Decrease credits after each post generation
       await db.collection("profiles").updateOne(
@@ -198,6 +103,7 @@ export const POST = withApiAuthRequiredExtended(
           $inc: { credits: -100 },
         }
       );
+      */
 
       return NextResponse.json({ success: true, post }, { status: 200 });
     } catch (error) {
@@ -206,4 +112,3 @@ export const POST = withApiAuthRequiredExtended(
     }
   }
 );
-*/
