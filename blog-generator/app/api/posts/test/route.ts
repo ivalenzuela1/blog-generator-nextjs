0ea2aa79fd6615f9dca36cdc18e3acab2e0bce6f
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-11-03 15:07:17 -0700

    Create generatePostEdge using an edge function

--- blog-generator/app/api/posts/test/route.ts
+++ blog-generator/app/api/posts/test/route.ts
@@ -6,11 +6,14 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
     const body = await request.json();
     const { description, keywords, tone, title } = body as PostPrompt;
 
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY as string,
-    });
-
-    const generateTitle = await openai.chat.completions.create({
+    const titlePayload = {
+      model: "gpt-4",
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 200,
+      n: 1,
       messages: [
         { role: "system", content: "You are a blog post writer" },
         {
@@ -22,13 +25,31 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
           }}. Do not wrap the title in quotes.`,
         },
       ],
+    };
+
+    const generateTitle = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+        },
+        method: "POST",
+        body: JSON.stringify(titlePayload),
+      }
+    );
+
+    const titleJSON = await generateTitle.json();
+    const titleResponse = titleJSON.choices[0].message.content as string;
+
+    const promptPayload = {
       model: "gpt-4",
-      temperature: 0.2,
-    });
-
-    const titleResponse = generateTitle.choices[0].message.content as string;
-
-    const postGenerator = await openai.chat.completions.create({
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 200,
+      n: 1,
       messages: [
         {
           role: "system",
@@ -36,42 +57,26 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
         },
         {
           role: "user",
-          content: `Write me a short and interesting blog post about ${description}. The title of the article is as follows: ${titleResponse}. These are the keywords for the post: ${keywords}. The blog post should be short and SEO friendly. The tone of the post should be ${tone}. Write it as well as you can. Do not include the title in the post, just start writing the post. Divide the post into paragraphs and write at least 2 short paragraphs. Distinguish the paragraphs with a line break.`,
+          content: `Write me a long and interesting blog post about ${description}. The title of the article is as follows: ${titleResponse}. These are the keywords for the post: ${keywords}. The blog post should be long and SEO friendly. The tone of the post should be ${tone}. Write it as well as you can. Do not include the title in the post, just start writing the post. Divide the post into paragraphs and write at least 3 paragraphs. Distinguish the paragraphs with a line break.`,
         },
       ],
-      model: "gpt-4",
-      temperature: 0.2,
-    });
+    };
 
-    let postResponse: string = "";
-    try {
-      postResponse = postGenerator.choices[0].message.content as string;
-    } catch (e) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: `Error getting content from generatePost: ${JSON.stringify(
-            e
-          )}`,
+    const _generatePrompt = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
         },
-        { status: 400 }
-      );
-    }
+        method: "POST",
+        body: JSON.stringify(promptPayload),
+      }
+    );
 
-    let paragraphs: string[] = [];
-    try {
-      paragraphs = postResponse?.split("\n\n");
-    } catch (e) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: `Error getting content from paragraphs: ${JSON.stringify(
-            e
-          )}`,
-        },
-        { status: 400 }
-      );
-    }
+    const promptJSON = await _generatePrompt.json();
+    const postResponse = promptJSON.choices[0].message.content as string;
+    const paragraphs = postResponse?.split("\n\n");
 
     const post: Post = {
       title: titleResponse || "No title generated",
